c1017d1ead07b43554e4eae61d48cd123f09623e
git <git@github.com>
2026-04-21 00:10:11 +0800

    202604210010

--- up.sh
+++ up.sh
@@ -6,6 +6,11 @@ ZLIB='https://github.com/madler/zlib/releases/download/v$(getversion madler/zlib
 C_ARES='https://github.com/c-ares/c-ares/releases/download/v$(getversion c-ares/c-ares)/c-ares-$(getversion c-ares/c-ares).tar.gz'
 EXPAT='https://github.com/libexpat/libexpat/releases/download/$(getversion libexpat/libexpat)/expat-$(echo "$(getversion libexpat/libexpat)" | head -n 1 | sed 's/_/./g' | sed 's/R.//g').tar.bz2'
 LIBSSH2='https://github.com/libssh2/libssh2/releases/download/$(getversion libssh2/libssh2)/$(getversion libssh2/libssh2).tar.gz'
-OPENSSL='https://github.com/openssl/openssl/releases/download/$(getversion openssl/openssl)/$(getversion openssl/openssl).tar.gz'
+# OPENSSL='https://github.com/openssl/openssl/releases/download/$(getversion openssl/openssl)/$(getversion openssl/openssl).tar.gz'
+curl -s https://openssl-library.org/source/ \
+| grep -A2 '\[LTS\]' \
+| grep -oE 'openssl-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz' \
+| head -n1 \
+| awk '{print "https://github.com/openssl/openssl/releases/download/" $0 "/" $0}'
 SQLITE3='https://sqlite.org/$(curl -fsSL https://sqlite.org/download.html | grep -o '..../sqlite-autoconf-.*.tar.gz' | head -n 1)'
 JEMALLOC='https://github.com/jemalloc/jemalloc/releases/download/$(getversion jemalloc/jemalloc)/jemalloc-$(getversion jemalloc/jemalloc).tar.bz2'" > dependences
